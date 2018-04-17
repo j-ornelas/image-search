3e8355950c8824e6b7ca509c94090e04e5d14d1e
j-ornelas <johnnyoh1988@gmail.com>
2018-04-17 12:27:39 -0700

    Adds classes for styling to spellcheck override

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -3,13 +3,15 @@ import React from 'react';
 export default function Search (props){
   let confirmSearch;
   if (props.lastSpellChecked && props.lastSearched !== props.lastSpellChecked){
-    confirmSearch = (<div>showing results for {props.lastSpellChecked}. <span onClick={props.fetchImages.bind(this, true)}>Search for {props.lastSearched} instead.</span></div>)
+    confirmSearch = (
+      <div className="spell-check-override-text">Currently showing results for {props.lastSpellChecked}. <span className="spell-check-override-link" onClick={props.overrideSpellCheck}>Search for {props.lastSearched} instead</span></div>
+      )
     }
 
   return (
     <div>
         <input className="input-field" type="text" placeholder="Search for images..." onChange={props.handleSearchChange}></input>
-        <button className="input-field btn waves-effect waves-light" onClick={props.fetchImages.bind(this, false)}>Search</button>
+        <button className="input-field btn waves-effect waves-light" onClick={props.fetchImages.bind(this, false, 30)}>Search</button>
         {confirmSearch}
     </div>
   )
