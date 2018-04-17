087599cce0708fcaa72d8a3026318ed2ce9056e2
j-ornelas <johnnyoh1988@gmail.com>
2018-04-17 16:30:21 -0700

    Code cleanup

--- client/src/components/Result.jsx
+++ client/src/components/Result.jsx
@@ -4,15 +4,13 @@ import ModalImage from 'react-modal-image'
 
 export default function Result (props){
   return (
-    <span className="results col s12 m4 l2 image">
+    <span className="results col s12 m4 l2 result-image">
     <ModalImage
-      className="image"
+      className="result-image"
       small={props.image.display_sizes[1].uri}
       large={props.image.display_sizes[0].uri}
       alt={props.image.title}
       />
     </span>
-
-
   )
 };
