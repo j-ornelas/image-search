93083daede38c8658850e31b9a776b4c8180e44b
j-ornelas <johnnyoh1988@gmail.com>
2018-04-16 18:57:31 -0700

    Server now handles autocorrection instead of client

--- server/server.js
+++ server/server.js
@@ -5,21 +5,28 @@ const title =  `Image Search App`;
 const path = require('path');
 const bodyParser = require('body-parser');
 const axios = require('axios');
-
+const helpers = require('./../helpers/helpers.js')
 
 app.use('/', express.static(path.join(__dirname, '../client/public')));
 app.use('/', bodyParser.json());
 
+
 app.get('/images', function(req, res){
-  axios(`https://api.gettyimages.com/v3/search/images?fields=id,title,thumb,comp,referral_destinations&sort_order=best&phrase=cat
-`, {
+  let query = req.query.q
+  let spellChecked = helpers.spellCheck(query)
+  let url = `https://api.gettyimages.com/v3/search/images?fields=title,thumb,comp&sort_order=best&phrase=${spellChecked}`
+  axios.get(url, {
     headers: {
       'Api-Key': req.query.api
     }
   })
   .then((response) => {
-    console.log(response.data.images)
-    res.send(response.data.images)
+    let queryResponse = {
+      images: response.data.images,
+      query: query,
+      spellCheckedQuery: spellChecked
+    }
+    res.send(queryResponse)
   })
   .catch((error) => {
     console.log(error)
