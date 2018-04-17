332c4052c9241e1dbedf50fc0e9a0d46bf2685bd
j-ornelas <johnnyoh1988@gmail.com>
2018-04-17 00:34:42 -0700

    Adds confirmation to override search term

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 export default function Search (props){
   let confirmSearch;
   if (props.lastSpellChecked && props.lastSearched !== props.lastSpellChecked){
-    confirmSearch = (<div>showing results for {props.lastSpellChecked}. Search for {props.lastSearched} instead.</div>)
+    confirmSearch = (<div>showing results for {props.lastSpellChecked}. <span onClick={props.fetchImages.bind(this, true)}>Search for {props.lastSearched} instead.</span></div>)
     }
 
   return (
     <div>
-        <input onChange={props.handleSearchChange}></input>
-        <button onClick={props.fetchImages}></button>
+        <input className="input-field" type="text" placeholder="Search for images..." onChange={props.handleSearchChange}></input>
+        <button className="input-field btn waves-effect waves-light" onClick={props.fetchImages.bind(this, false)}>Search</button>
         {confirmSearch}
     </div>
   )
